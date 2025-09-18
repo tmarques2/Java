3905399c9cf9bd35209fd279030af117ad8b609e
tmarques2 <thainaramarques258@gmail.com>
2025-09-18 11:24:08 -0300

    lista2

--- LIsta2/src/ex4_lista2.java
+++ LIsta2/src/ex4_lista2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ex4_lista2 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in)
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Para onde deseja ir? \n1 - Maceió \n2 - Porto de Galinhas");
 
@@ -17,9 +17,25 @@ public class ex4_lista2 {
         scanner.nextLine();
 
         Double valor_base = 3000.00;
+        Double desconto;
+        Double valor_final;
 
         if (destino == 1 && comida == 1){
             System.out.printf("O valor da viagem será: %.2f", valor_base);
+        } else if (destino == 1 && comida == 2) {
+            desconto = valor_base * 0.85;
+            valor_final = valor_base - desconto;
+            System.out.printf("O valor da viagem será: %.2f", valor_final);
+        } else if (destino == 2 && comida == 1) {
+            desconto = valor_base * 0.6;
+            valor_final = valor_base - desconto;
+            System.out.printf("O valor da viagem será: %.2f", valor_final);
+        } else if (destino == 2 && comida == 2) {
+            desconto = valor_base * 0.45;
+            valor_final = valor_base - desconto;
+            System.out.printf("O valor da viagem será: %.2f", valor_final);
+        } else {
+            System.out.println("Essa opção não existe");
         }
     }
 }
