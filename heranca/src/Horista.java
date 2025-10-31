5aa3ccc17755a6be0b67701dc587d11f3896dc4b
tmarques2 <thainaramarques258@gmail.com>
2025-10-31 11:14:45 -0300

    sla

--- heranca/src/Horista.java
+++ heranca/src/Horista.java
@@ -4,5 +4,10 @@ public class Horista extends Colaborador{
 
     public Horista(int edv, String nome, String cargo, double horasTrabalhadas, double salarioPorHora){
         super(edv, nome, cargo);
+        this.horasTrabalhadas = horasTrabalhadas;
+        this.salarioPorHora = salarioPorHora;
+    }
+    double receberSalario(){
+        return this.salarioPorHora * this.horasTrabalhadas;
     }
 }
